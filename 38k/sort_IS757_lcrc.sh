fe86f0823c32e3ced8d5e0397697a4fb7ff2cfe8
calemhoffman <calem.hoffman@gmail.com>
2025-02-10 22:41:36 -0600

    Update sort_IS757_lcrc.sh
    First mod to attempt LCRC sorting

--- 38k/sort_IS757_lcrc.sh
+++ 38k/sort_IS757_lcrc.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # For sorting single runs during shifts for IS757
 # Samuel Reeve 03/10/2024
+# Mod for running on LCRC at ANL CRH - 10/02/2025
 
 # Directories
-BINDIR="/Users/calemhoffman/Documents/GitHub/ISSSort_crh/bin"
-CALDIR="/Users/calemhoffman/Research/isolde/cals"
-DATADIR="/Users/calemhoffman/Research/isolde/data"
+BINDIR="/home/crhoffman/iss_crh/bin"
+CALDIR="/home/crhoffman/iss_crh/38k"
+DATADIR="/lcrc/project/HELIOS/experimentsData/IS757"
 
 # Calibration files
-SETTINGSFILE="${CALDIR}/settings_38K_031024.dat"
-CALIBFILE="${CALDIR}/calibration_IS757_031024.cal"
+SETTINGSFILE="${CALDIR}/settings_38K_IS757.dat"
+CALIBFILE="${CALDIR}/calibration_38K_IS757.cal"
 REACTIONFILE="${CALDIR}/reaction_38K_IS757.dat"
 #CUTFILE="${CALDIR}/2MeV_EvZ_thres.root"-> Set In Reaction File
 
@@ -25,4 +26,4 @@ fi
 # Second argument = iss_sort flag (e.g. -e, -f)
 FLAG=$2
 
-./iss_sort -s $SETTINGSFILE -c $CALIBFILE -r $REACTIONFILE -i $DATADIR/R${RUN}_?? $DATADIR/R${RUN}_? ${FLAG} -o $DATADIR/sorted/R${RUN}_sumhists.root
+${BINDIR}/iss_sort -s $SETTINGSFILE -c $CALIBFILE -r $REACTIONFILE -i $DATADIR/R${RUN}_?? $DATADIR/R${RUN}_? ${FLAG} -o $DATADIR/sorted_crh/R${RUN}_sumhists.root
